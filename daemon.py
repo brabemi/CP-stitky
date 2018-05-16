305c6b6f71167d8b0d50fc772465952925f12e4b
Miroslav Brabenec <brabemi@gmail.com>
2018-05-16 15:44:23 +0200

    Daemonize  version

--- daemon.py
+++ daemon.py
@@ -1,8 +1,20 @@
 import base64
 import io
+import os
+from configparser import ConfigParser
+from sys import stderr
 
 import barcode
-from flask import Flask, make_response
+import click
+import flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlsoup import SQLSoup
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.python.threadpool import ThreadPool
+from twisted.web.server import Site
+from twisted.web.wsgi import WSGIResource
 from weasyprint import HTML
 
 HTML_TEMPLATE = '''
@@ -274,9 +286,7 @@ def calculate_pkg_checksum(number):
     for factor in FACTORS[::-1]:
         number, module = divmod(number, 10)
         checksum += factor * module
-        # print(checksum, factor, factor * module)
     a = checksum % 11
-    # print(checksum, a)
     if a > 1:
         return 11 - a
     if a == 1:
@@ -287,7 +297,6 @@ def calculate_pkg_checksum(number):
 def create_pkg_id(prefix, postfix, submitter_id, pkg_number):
     tmp_pkg_number = '{:0{}d}'.format(pkg_number, PKG_PADDING - len(submitter_id))
     checksum = calculate_pkg_checksum(int('{}{}'.format(submitter_id, tmp_pkg_number)))
-    # print(tmp_pkg_number)
     return '{}{}{}{}{}'.format(
         prefix, submitter_id, tmp_pkg_number, checksum, postfix
     )
@@ -314,22 +323,93 @@ def generate_pdf(src_to_dst, dst_to_src):
     result = html.write_pdf()
     return result
 
-app = Flask(__name__)
 
-@app.route('/')
-def stitek_cp():
-    src_to_dst = {
-        'package_id': create_pkg_id(PKG_PREFIX, PKG_POSTFIX, SUBMITTER_ID, PKG_S2D),
-        'sender': SOURCE,
-        'addressee': DEST,
-    }
-    dst_to_src = {
-        'package_id': create_pkg_id(PKG_PREFIX, PKG_POSTFIX, SUBMITTER_ID, PKG_D2S),
-        'sender': DEST,
-        'addressee': SOURCE,
-    }
-    pdf = generate_pdf(src_to_dst, dst_to_src)
-    response = make_response(pdf)
-    response.headers.set('Content-Disposition', 'attachment', filename='stitek.pdf')
-    response.headers.set('Content-Type', 'application/pdf')
-    return response
+def make_site(db, debug=False):
+    app = flask.Flask('.'.join(__name__.split('.')[:-1]))
+    app.secret_key = os.urandom(16)
+    app.debug = debug
+
+    @app.route('/')
+    def postal_label():
+        src_to_dst = {
+            'package_id': create_pkg_id(PKG_PREFIX, PKG_POSTFIX, SUBMITTER_ID, PKG_S2D),
+            'sender': SOURCE,
+            'addressee': DEST,
+        }
+        dst_to_src = {
+            'package_id': create_pkg_id(PKG_PREFIX, PKG_POSTFIX, SUBMITTER_ID, PKG_D2S),
+            'sender': DEST,
+            'addressee': SOURCE,
+        }
+        pdf = generate_pdf(src_to_dst, dst_to_src)
+        response = flask.make_response(pdf)
+        response.headers.set('Content-Disposition', 'attachment', filename='stitek.pdf')
+        response.headers.set('Content-Type', 'application/pdf')
+        return response
+
+    return app
+
+@click.command()
+@click.option(
+    '--config',
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False,
+        writable=False, readable=True, resolve_path=True
+    ),
+    help='path to config file',
+)
+def main(config):
+    cfg = ConfigParser()
+    cfg.read(config)
+
+    # Start Twisted logging to console.
+    log.startLogging(stderr)
+
+    # Read database configuration options.
+    db_url = cfg.get('database', 'url')
+
+    # Read website configuration options.
+    http_debug = cfg.getboolean('http', 'debug', fallback=False)
+    http_host = cfg.get('http', 'host', fallback='localhost')
+    http_port = cfg.getint('http', 'port', fallback=5000)
+    http_pool = cfg.getint('http', 'pool_size', fallback=4)
+
+    # Default to much saner database query defaults and always
+    # commit and/or flush statements explicitly.
+    # factory = sessionmaker(autocommit=False, autoflush=False)
+
+    # Prepare database connection with table reflection.
+    engine = create_engine(db_url)
+    session = scoped_session(sessionmaker(autocommit=False, autoflush=False))
+    db = SQLSoup(engine, session=session)
+
+    # Extract manager options, sans the pool_size we handle here.
+    # pool_size = int(manager_opts.pop('pool_size', 2))
+    pool_size = 2
+
+    # Set the correct thread pool size for the manager.
+    reactor.suggestThreadPoolSize(pool_size)
+
+    # Prepare the website that will get exposed to the users.
+    site = make_site(db, debug=http_debug)
+
+    # Prepare WSGI site with a separate thread pool.
+    pool = ThreadPool(http_pool, http_pool, 'http')
+    site = Site(WSGIResource(reactor, pool, site))
+    pool.start()
+
+    # Bind the website to it's address.
+    reactor.listenTCP(http_port, site, interface=http_host)
+
+    # Run the Twisted reactor until the user terminates us.
+    reactor.run()
+
+    # Kill the HTTP ThreadPool.
+    pool.stop()
+
+if __name__ == '__main__':
+    main()
+
+
+# vim:set sw=4 ts=4 et:
+# -*- coding: utf-8 -*-
